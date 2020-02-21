bcd1253f2e86446317bebf9fc29149212e008d1d
Maxime Charlebois <mcharlebois@ccqlin025.flatironinstitute.org>
2020-02-21 11:15:05 -0500

    need factorial or factorial square?

--- c++/inchworm/mc/measures/u_frame.cpp
+++ c++/inchworm/mc/measures/u_frame.cpp
@@ -2,13 +2,23 @@
 
 namespace inchworm::measures {
 
+  // calculate n!
+  //
+  inline int factorial(int n) {
+    if (n > 1)
+      return n * factorial(n - 1);
+    else
+      return 1;
+  }
+
   //u_frame::u_frame(params_t const &, qmc_config_data_t const &qmc_config_data_, container_set &results_)
   u_frame::u_frame(params_t const &, qmc_config_data_t const &qmc_config_data_, single_step_results_t &results_)
      : qmc_config_data(qmc_config_data_), results(results_) {}
 
   void u_frame::accumulate(scalar_t sign) {
     //int factor = 1;
-    scalar_t s = sign / (qmc_config_data.w.loc);
+    int factor = factorial(results.u_frame.size());
+    scalar_t s = sign / (qmc_config_data.w.loc * factor);
     //if (qmc_config_data.config.size() < MAX_ORDER) { results.u_expansion_order[qmc_config_data.config.size()] += 1 / qmc_config_data.w.loc; }
     //average_sign += s;
     //if (qmc_config_data.config.size() == 1)
