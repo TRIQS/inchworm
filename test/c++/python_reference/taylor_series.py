810bba3bbcd2a8b6a585d4523277fb1df4333747
Maxime Charlebois <mnmpdadish@hotmail.com>
2020-02-25 19:12:26 -0500

    adding taylor_series.py

---
+++ test/c++/python_reference/taylor_series.py
@@ -0,0 +1,25 @@
+
+import sympy as sy
+import numpy as np
+from sympy.functions import cosh
+
+theta = sy.Symbol('theta')
+beta = sy.Symbol('beta')
+f = (cosh(beta*theta/2))**4
+
+def factorial(n):
+  if n <= 0:
+    return 1
+  else:
+    return n*factorial(n-1)
+
+def taylor(function,theta0,N):
+  n = 0
+  serie = 0
+  while n <= N:
+    serie = serie + (function.diff(theta,n).subs(theta,theta0))/(factorial(n))*(theta-theta0)**n
+    n += 1
+  return serie
+    
+print taylor(f,0,8)
+
