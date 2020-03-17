8682b739d73d018efe6a583b8652161c40074909
Maxime Charlebois <mnmpdadish@hotmail.com>
2020-03-17 15:06:38 -0400

    sympy cannot do this integral

--- test/c++/python_reference/integral1.py
+++ test/c++/python_reference/integral1.py
@@ -8,6 +8,8 @@ theta = sy.Symbol('theta')
 beta = sy.Symbol('beta')
 tau1 = sy.Symbol('tau_1')
 tau2 = sy.Symbol('tau_2')
+
+taus = sy.Symbol('tau_s')
 f = (cosh(beta*theta/2))**2
 
 U = sy.Function('U')(beta)
@@ -15,11 +17,20 @@ U = sy.Function('U')(beta)
 #sy.pprint(sy.integrate(x/(x**2+2*x+1), x))
 print U
 
-sy.pprint(sy.integrate(cosh((tau1)*theta/2)**2, tau1))
+#sy.pprint(sy.integrate(cosh((tau1)*theta/2)**2, tau1))
 
 #sy.pprint(sy.integrate( sy.integrate( (cosh((beta-tau1)*theta/2))**2  * (cosh((tau1-tau2)*theta/2))**2  * (cosh((tau2)*theta/2))**2   , tau1)  , tau2 ))
 
+sy.pprint( sy.integrate( sy.integrate( \
+            sy.expand((exp((beta-tau1)*theta/2) + exp(-(beta-tau1)*theta/2) + 2) * \
+                     (exp((tau1-tau2)*theta/2) + exp(-(tau1-tau2)*theta/2) + 2) * \
+                     (exp((tau2)*theta/2) + exp(-(tau2)*theta/2) + 2) ) \
+                     , (tau1,0,taus) ),   (tau2,taus,beta) ) )
 
 
 exit()
 
+
+from sympy import *
+x, y = symbols('x, y')
+integrate(x + y)
