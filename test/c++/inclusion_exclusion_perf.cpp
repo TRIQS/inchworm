e8da5d5152aaa1fe5bc18a50c36cb8e40e151cb4
Maxime Charlebois <mnmpdadish@hotmail.com>
2020-11-26 14:57:28 -0500

    3m47s time for 500000 times order 5 to 10m

--- test/c++/inclusion_exclusion_perf.cpp
+++ test/c++/inclusion_exclusion_perf.cpp
@@ -51,10 +51,10 @@ std::vector<double> generate_random_vector(double beta, int n_tau) {
 }
 
 int main(void) {
-  int N         = 200;//200;
+  int N         = 500000;//200;
   int sp_max    = 1;
-  int order_min = 22; // order 0 is a special case that fails for now.
-  int order_max = 22; // order 9 and above are quite slow
+  int order_min = 5; // order 0 is a special case that fails for now.
+  int order_max = 10; // order 9 and above are quite slow
   double beta   = 1.0;
 
   for (int order = order_min; order <= order_max; order++)
