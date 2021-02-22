81ed0b16c597af222c3307dae001d6375e72ce4c
Nils Wentzell <nwentzell@flatironinstitute.org>
2021-02-22 15:25:01 -0500

    Add special treatment for insertion into zeroth order inchworm config

--- c++/inchworm/moves/insert.cpp
+++ c++/inchworm/moves/insert.cpp
@@ -7,13 +7,27 @@ namespace inchworm::moves {
     auto d     = all_d_ops[rng(n_fops)];
     auto d_dag = all_d_dag_ops[rng(n_fops)];
 
-    d.tau     = rng(params.tau_max);
-    d_dag.tau = rng(params.tau_max);
+    scalar_t t_ratio;
+    int NewSize = config.size() + 1;
+    if (NewSize == 1 && (params.tau_split != 0.0)) {
+      // Special case of empty configuration for inchworm
+      // Make sure that we choose tau values on seperate
+      // sides of the split point
+
+      auto dtau = params.tau_max - params.tau_split;
+      d.tau     = rng(params.tau_split);
+      d_dag.tau = params.tau_split + rng(dtau);
+      if (rng(2)) std::swap(d.tau, d_dag.tau);
+      t_ratio = 2.0 * params.tau_split * dtau * n_fops * n_fops / NewSize / NewSize;
+    } else {
+      d.tau     = rng(params.tau_max);
+      d_dag.tau = rng(params.tau_max);
+      t_ratio   = std::pow(params.tau_max * n_fops / NewSize, 2);
+    }
 
     if (not config.try_insert(d_dag, d)) return 0.0;
 
-    int N = config.size();
-    return std::pow(params.tau_max * n_fops / N, 2);
+    return t_ratio;
   }
 
   scalar_t double_insert::try_config_update(config_t &config) {
--- c++/inchworm/moves/remove.cpp
+++ c++/inchworm/moves/remove.cpp
@@ -3,16 +3,23 @@
 namespace inchworm::moves {
 
   scalar_t remove::try_config_update(config_t &config) {
-    int N = config.size();
-    if (N == 0) return 0;
+    int OldSize = config.size();
+    if (OldSize == 0) return 0;
 
-    int idx     = rng(N);
-    int idx_dag = rng(N);
+    int idx     = rng(OldSize);
+    int idx_dag = rng(OldSize);
 
     if (not config.try_erase(idx_dag, idx)) return 0;
 
     int n_fops = all_d_ops.size();
-    return std::pow(N / (n_fops * params.tau_max), 2);
+
+    // Account for special treatment of insertion into empty configuration
+    if (OldSize == 1 && (params.tau_split != 0.0)) {
+      auto dtau = params.tau_max - params.tau_split;
+      return 1.0 * OldSize * OldSize / (2.0 * params.tau_split * dtau * n_fops * n_fops);
+    } else {
+      return std::pow(OldSize / (n_fops * params.tau_max), 2);
+    }
   }
 
   scalar_t double_remove::try_config_update(config_t &config) {
