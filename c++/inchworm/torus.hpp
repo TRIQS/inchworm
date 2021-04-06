9d3c8318ce680977a9637062937a3a385149e8fd
Nils Wentzell <nwentzell@flatironinstitute.org>
2021-04-06 10:52:30 -0400

    Add torus.hpp for wrap, cyclic_difference and cyclic_distance

---
+++ c++/inchworm/torus.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "./types.hpp"
+#include "./config.hpp"
+
+namespace inchworm {
+
+  // Find the standard representative of tau on the torus R + Z * tau_max
+  inline double wrap(double tau, double tau_max) { return (tau > 0) ? std::fmod(tau, tau_max) : tau_max - std::fmod(-tau, tau_max); }
+
+  // Return the difference of the times on the torus R + Z * tau_max
+  inline double cyclic_difference(double tau1, double tau2, double tau_max) { return wrap(tau1 - tau2, tau_max); }
+
+  // Return the shortest absolute distance of the times on the torus R + Z * tau_max
+  inline double cyclic_distance(double tau1, double tau2, double tau_max) {
+    auto wrap_abs = wrap(std::abs(tau1 - tau2), tau_max);
+    return std::min(wrap_abs, tau_max - wrap_abs);
+  }
+
+  // Return the cyclic_difference between the creation/annihilation operator
+  // and the closest annihilation/creation operator of the same block of the config
+  inline double cyclic_difference(auto const &op, config_t const &config, double tau_max) {
+    if (config.size(op.bl) == 0) TRIQS_RUNTIME_ERROR << "cyclic_difference requires the operator block to no be empty";
+
+    double min_dist = tau_max;
+    double res      = tau_max;
+    for (auto const &opdiag : op.dag ? config.d_bl_list[op.bl] : config.d_dag_bl_list[op.bl]) {
+      auto diff = cyclic_difference(op.tau, opdiag.tau, tau_max);
+      auto dist = cyclic_distance(op.tau, opdiag.tau, tau_max);
+      if (dist < min_dist) {
+        min_dist = dist;
+        res      = diff;
+      }
+    }
+    return res;
+  }
+
+} // namespace inchworm
---
+++ test/c++/torus.cpp
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ *
+ * TRIQS: a Toolbox for Research in Interacting Quantum Systems
+ *
+ * Copyright (C) 2020 Simons Foundation
+ *   author: N. Wentzell
+ *
+ * TRIQS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * TRIQS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * TRIQS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#include <nda/gtest_tools.hpp>
+
+#include <inchworm/torus.hpp>
+#include <inchworm/config.hpp>
+
+using namespace inchworm;
+
+TEST(Torus, Wrap) {
+  EXPECT_CLOSE(wrap( 2.0, 1.0), 0.0);
+  EXPECT_CLOSE(wrap( 1.3, 1.0), 0.3);
+  EXPECT_CLOSE(wrap( 1.7, 1.0), 0.7);
+  EXPECT_CLOSE(wrap(-1.3, 1.0), 0.7);
+  EXPECT_CLOSE(wrap(-1.7, 1.0), 0.3);
+}
+
+TEST(Torus, CyclicDifference) {
+  EXPECT_CLOSE(cyclic_difference(0.3, 0.1, 1.0), 0.2);
+  EXPECT_CLOSE(cyclic_difference(0.1, 0.3, 1.0), 0.8);
+
+  EXPECT_CLOSE(cyclic_difference(1.2, 1.1, 1.0), 0.1);
+  EXPECT_CLOSE(cyclic_difference(1.1, 1.2, 1.0), 0.9);
+
+  EXPECT_CLOSE(cyclic_difference(2.4, 0.1, 2.0), 0.3);
+  EXPECT_CLOSE(cyclic_difference(2.1, 0.4, 2.0), 1.7);
+
+  EXPECT_CLOSE(cyclic_difference(0.5, 2.1, 2.0), 0.4);
+  EXPECT_CLOSE(cyclic_difference(0.1, 2.5, 2.0), 1.6);
+}
+
+TEST(Torus, CyclicDifferenceConfig) {
+  // ------------ tau,   dag, i, bl 
+  auto d1 = fop_t{0.1, false, 0, 0};
+  auto d2 = fop_t{0.8, false, 0, 0};
+  auto d3 = fop_t{0.4, false, 0, 1};
+
+  auto d_dag1 = fop_t{0.4, true, 0, 0};
+  auto d_dag2 = fop_t{0.7, true, 0, 0};
+  auto d_dag3 = fop_t{0.8, true, 0, 1};
+
+  config_t config({}, {{"0", 1}, {"1", 1}});
+  config.d_bl_list = {{d1, d2},{d3}};
+  config.d_dag_bl_list = {{d_dag1, d_dag2},{d_dag3}};
+
+  EXPECT_CLOSE(cyclic_difference(d1, config, 1.0), 0.7);
+  EXPECT_CLOSE(cyclic_difference(d2, config, 1.0), 0.1);
+  EXPECT_CLOSE(cyclic_difference(d3, config, 1.0), 0.6);
+
+  EXPECT_CLOSE(cyclic_difference(d_dag1, config, 1.0), 0.3);
+  EXPECT_CLOSE(cyclic_difference(d_dag2, config, 1.0), 0.9);
+  EXPECT_CLOSE(cyclic_difference(d_dag3, config, 1.0), 0.4);
+}
+
+TEST(Torus, CyclicDistance) {
+  EXPECT_CLOSE(cyclic_distance(0.1, 0.2, 1.0), 0.1);
+  EXPECT_CLOSE(cyclic_distance(0.2, 0.1, 1.0), 0.1);
+}
