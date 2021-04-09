f81f2825ac7f14e2755cf0ffa1fc361a4f0d1d9f
Nils Wentzell <nwentzell@flatironinstitute.org>
2021-04-09 12:47:12 -0400

    Stability improvements of pdf, extend distributions test

--- c++/inchworm/distributions.hpp
+++ c++/inchworm/distributions.hpp
@@ -13,10 +13,11 @@ namespace inchworm {
   // Inverse of the cumulative distribution function associated
   // with the truncated exponential probability density
   inline double icdf(double p, double w, double tmax) {
-    EXPECTS(0.0 < std::exp(-tmax / w));
     // Explicitly treat small tmax / w to avoid instabilities
     if (tmax / w < 1e-8) return p * tmax;
-    return -w * std::log(1.0 - p + p * std::exp(-tmax / w));
+    double exp_val = std::exp(-tmax / w);
+    if ((1.0 - p) * 1e+3 <= exp_val) return tmax;
+    return -w * std::log(1.0 - p + p * exp_val);
   }
 
   // Probability density function on the interval [0,tmax] combining
--- test/c++/distributions.cpp
+++ test/c++/distributions.cpp
@@ -48,25 +48,29 @@ TEST(Distributions, pdf) {
   }
 }
 
+double integrate(auto f, double tmin, double tmax, long Nint = 1e+6) {
+  double dtau    = (tmax - tmin) / Nint;
+  double int_pdf = 0.0;
+  for (auto i : range(Nint)) int_pdf += f(tmin + (0.5 + i) * dtau) * dtau;
+  return int_pdf;
+}
+
 TEST(Distributions, norm_pdf) {
 
   std::vector<double> tmaxlst = {1.0, 10.0, 100.0};
-  std::vector<double> wlst    = {0.1, 10.0, 1000.0};
+  std::vector<double> wlst    = {0.1, 10.0, 1000.0, 1e+8};
 
   for (auto tmax : tmaxlst) {
     for (auto w1 : wlst) {
 
-      long Nint = 1e+6;
-      double dtau = tmax / Nint;
-
-      double int_pdf        = 0.0;
-      for (auto i: range(Nint)) int_pdf += pdf((0.5 + i) * dtau, w1, tmax) * dtau;
-      EXPECT_NEAR(int_pdf, 1.0, 1.0/Nint);
+      auto f         = [&](double tau) { return pdf(tau, w1, tmax); };
+      double int_pdf = integrate(f, 0, tmax);
+      EXPECT_NEAR(int_pdf, 1.0, 1.0 / 1e+6);
 
       for (auto w2 : wlst) {
-        double int_double_pdf = 0.0;
-        for (auto i: range(Nint)) int_double_pdf += double_pdf((0.5 + i) * dtau, w1, w2, tmax) * dtau;
-        EXPECT_NEAR(int_double_pdf, 1.0, 1.0/Nint);
+        auto g                = [&](double tau) { return double_pdf(tau, w1, w2, tmax); };
+        double int_double_pdf = integrate(g, 0, tmax);
+        EXPECT_NEAR(int_double_pdf, 1.0, 1.0 / 1e+6);
       }
     }
   }
@@ -74,13 +78,13 @@ TEST(Distributions, norm_pdf) {
 
 TEST(Distributions, icdf) {
 
-  std::vector<double> tmaxlst = {1.0, 10.0};
-  std::vector<double> wlst    = {0.1, 10.0, 100.0};
+  std::vector<double> tmaxlst = {1.0, 10.0, 100.0};
+  std::vector<double> wlst    = {0.1, 10.0, 1000.0, 1e+6};
 
   for (auto tmax : tmaxlst) {
     for (auto w1 : wlst) {
 
-      double tol = 1e-14;
+      double tol = 1e-10;
 
       EXPECT_NEAR(icdf(0.0, w1, tmax), 0.0, tol);
       EXPECT_NEAR(icdf(1.0, w1, tmax), tmax, tol);
@@ -90,6 +94,108 @@ TEST(Distributions, icdf) {
 	EXPECT_NEAR(double_icdf(0.5, w1, w2, tmax), tmax/2.0, tol);
 	EXPECT_NEAR(double_icdf(1.0, w1, w2, tmax), tmax, tol);
       }
+    }
+  }
+}
+
+TEST(Distributions, icdf_inverse) {
+
+  std::vector<double> tmaxlst = {1.0, 10.0, 100.0};
+  std::vector<double> wlst    = {0.1, 10.0, 1000.0, 1e+6};
+
+  for (auto tmax : tmaxlst) {
+    for (auto w1 : wlst) {
+
+      // -- Calculate the cumulative distrubition function numerically
+
+      auto f   = [&](double tau) { return pdf(tau, w1, tmax); };
+      auto cdf = [&](double tau) { return integrate(f, 0, tau); };
+
+      // -- Check that inverse holds both directions
+
+      double tol = 1e-6;
+      EXPECT_NEAR((cdf(icdf(0.2, w1, tmax))), 0.2, tol);
+      EXPECT_NEAR((cdf(icdf(0.5, w1, tmax))), 0.5, tol);
+      EXPECT_NEAR((cdf(icdf(0.7, w1, tmax))), 0.7, tol);
+
+      tol = 1e-6 * tmax;
+      // CAUTION: Do not evaluate deep in the exponential tail of the pdf
+      if (w1 < tmax)
+        EXPECT_NEAR((icdf(cdf(w1), w1, tmax)), w1, tol);
+      else
+        EXPECT_NEAR((icdf(cdf(tmax), w1, tmax)), tmax, tol);
+
+      if (5.0 * w1 < tmax)
+        EXPECT_NEAR((icdf(cdf(5.0 * w1), w1, tmax)), 5.0 * w1, tol);
+      else
+        EXPECT_NEAR((icdf(cdf(0.2 * tmax), w1, tmax)), 0.2 * tmax, tol);
+
+      if (10.0 * w1 < tmax)
+        EXPECT_NEAR((icdf(cdf(10.0 * w1), w1, tmax)), 10.0 * w1, tol);
+      else
+        EXPECT_NEAR((icdf(cdf(0.1 * tmax), w1, tmax)), 0.1 * tmax, tol);
+
+      for (auto w2 : wlst) {
+
+        // -- Calculate the cumulative distrubition function numerically
+
+        auto g          = [&](double tau) { return double_pdf(tau, w1, w2, tmax); };
+        auto double_cdf = [&](double tau) { return integrate(g, 0, tau); };
+
+        // -- Check that inverse holds both directions
+
+        tol = 1e-6;
+        EXPECT_NEAR((double_cdf(double_icdf(0.2, w1, w2, tmax))), 0.2, tol);
+        EXPECT_NEAR((double_cdf(double_icdf(0.5, w1, w2, tmax))), 0.5, tol);
+        EXPECT_NEAR((double_cdf(double_icdf(0.7, w1, w2, tmax))), 0.7, tol);
+
+        tol = 1e-6 * tmax;
+        if (w1 < tmax / 2.0)
+          EXPECT_NEAR((double_icdf(double_cdf(w1), w1, w2, tmax)), w1, tol);
+        else
+          EXPECT_NEAR((double_icdf(double_cdf(0.4 * tmax), w1, w2, tmax)), 0.4 * tmax, tol);
+
+        if (w2 < tmax / 2.0)
+          EXPECT_NEAR((double_icdf(double_cdf(tmax - w2), w1, w2, tmax)), tmax - w2, tol);
+        else
+          EXPECT_NEAR((double_icdf(double_cdf(0.6 * tmax), w1, w2, tmax)), 0.6 * tmax, tol);
+
+        if (5.0 * w1 < tmax / 2.0)
+          EXPECT_NEAR((double_icdf(double_cdf(5.0 * w1), w1, w2, tmax)), 5.0 * w1, tol);
+        else
+          EXPECT_NEAR((double_icdf(double_cdf(0.1 * tmax), w1, w2, tmax)), 0.1 * tmax, tol);
+
+        if (5.0 * w2 < tmax / 2.0)
+          EXPECT_NEAR((double_icdf(double_cdf(tmax - 5.0 * w2), w1, w2, tmax)), tmax - 5.0 * w2, tol);
+        else
+          EXPECT_NEAR((double_icdf(double_cdf(0.9 * tmax), w1, w2, tmax)), 0.9 * tmax, tol);
+      }
+    }
+  }
+}
+
+TEST(Distributions, get_prob) {
+
+  std::vector<double> tmaxlst = {1.0, 10.0, 100.0};
+  std::vector<double> wlst    = {0.1, 10.0, 1000.0, 1e+8};
+
+  for (auto tmax : tmaxlst) {
+    for (auto w1 : wlst) {
+
+      auto d1 = fop_t{0.1 * tmax, false, 0, 0, 0, w1, w1};
+
+      auto d_dag1 = fop_t{0.4 * tmax, true, 0, 0, 0, w1, w1};
+      auto d_dag2 = fop_t{0.7 * tmax, true, 0, 0, 0, w1, w1};
+      auto d_dag3 = fop_t{0.9 * tmax, true, 0, 1, 0, w1, w1};
+
+      auto op_list = std::vector{d_dag1, d_dag2, d_dag3};
+
+      double tol = 1e-6;
+      auto f     = [&](double tau) {
+        d1.tau = tau;
+        return get_prob(d1, op_list, tmax);
+      };
+      EXPECT_NEAR(integrate(f, 0.0, tmax), 1.0, tol);
     }
   }
 }
