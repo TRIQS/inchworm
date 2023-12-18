039b5eab84f7f32652b8fa54f8850df4af96979a
umyangyu <umyangyu@umich.edu>
2023-12-18 10:42:44 -0500

    use myAddPivots

--- apps/mode/mode_partition_sin.cpp
+++ apps/mode/mode_partition_sin.cpp
@@ -154,7 +154,7 @@ void ModePartitionSin::run_single_element() {
         std::cout << "iteration nEval LastSweepPivotError integral\n";
         auto ci = xfac::CTensorCI2<double, double>(get_u_tau_max_element, input,
                                                    {.bondDim = tp.bond_dim, .reltol = 1e-18, .pivot1 = pivot1, .fullPiv = true});
-        for (auto b = 0u; b < ci.len() - 1; b++) { ci.addPivotsAt(ci_pre.getPivotsAt(b), b); }
+        for (auto b = 0u; b < ci.len() - 1; b++) { ci.myAddPivotsAt(ci_pre.getPivotsAt(b), b); }
         // ci.makeCanonical();
         print_rank(ci.tt);
         std::cout << "bond_dim: " << ci.param.bondDim << std::endl;
