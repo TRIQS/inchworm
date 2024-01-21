745920cb5ec4e028452223b593c49c2aea7b05ff
Yang Yu <umyangyu@umich.edu>
2024-01-20 19:56:48 -0500

    modify the reltol

--- apps/mode/mode_partition_sin.cpp
+++ apps/mode/mode_partition_sin.cpp
@@ -123,7 +123,7 @@ void ModePartitionSin::run_single_element() {
         input_pre.insert(input_pre.end(), input_to_append_pre.begin(), input_to_append_pre.end());
         std::cout << "iteration nEval LastSweepPivotError\n";
         auto ci_pre = xfac::CTensorCI2<double, double>(get_u_tau_max_element_pre, input_pre,
-                                                       {.bondDim = tp.bond_dim, .reltol = 1e-30, .pivot1 = pivot1, .fullPiv = true});
+                                                       {.bondDim = tp.bond_dim, .reltol = 1e-20, .pivot1 = pivot1, .fullPiv = true});
         std::cout << "bond_dim: " << ci_pre.param.bondDim << std::endl;
         int ci_count                = 0;
         double previous_pivot_error = -1E5;
@@ -156,7 +156,7 @@ void ModePartitionSin::run_single_element() {
 
         std::cout << "iteration nEval LastSweepPivotError integral\n";
         auto ci = xfac::CTensorCI2<double, double>(get_u_tau_max_element, input,
-                                                   {.bondDim = tp.bond_dim, .reltol = 1e-18, .pivot1 = pivot1, .fullPiv = true});
+                                                   {.bondDim = tp.bond_dim, .reltol = 1e-20, .pivot1 = pivot1, .fullPiv = true});
         // for (auto b = 0u; b < ci.len() - 1; b++) { ci.myAddPivotsAt(ci_pre.getPivotsAt(b), b); }
         ci.addPivots(ci_pre);
         ci.makeCanonical();
