ff9b75ed7928857ce9e4fb322e763796e9c8017c
Yang Yu <umyangyu@umich.edu>
2024-01-30 13:53:40 -0500

    increase the cost of pretrain

--- apps/mode/mode_partition_sin_half.cpp
+++ apps/mode/mode_partition_sin_half.cpp
@@ -77,7 +77,7 @@ void ModePartitionSinHalf::run_single_element() {
       std::cout << ci_count_auxiliary << " " << count_auxiliary << " " << last_pivot_error << " " << std::endl;
       print_rank(ci_pre_auxiliary.tt);
       ci_count_auxiliary++;
-      if (std::abs(last_pivot_error - previous_pivot_error_auxiliary) < tci_convergence_threshold && ci_count_auxiliary > 3) { break; }
+      if (std::abs(last_pivot_error - previous_pivot_error_auxiliary) < tci_convergence_threshold && ci_count_auxiliary > 4) { break; }
       previous_pivot_error_auxiliary = last_pivot_error;
     }
     double integral_auxiliary = ci_pre_auxiliary.tt.sum(weight_pre);
@@ -218,7 +218,7 @@ void ModePartitionSinHalf::run_single_element() {
           std::cout << ci_count << " " << count_pre << " " << last_pivot_error << " " << std::endl;
           print_rank(ci_pre.tt);
           ci_count++;
-          if (std::abs(last_pivot_error - previous_pivot_error) < tci_convergence_threshold && ci_count > 3) { break; }
+          if (std::abs(last_pivot_error - previous_pivot_error) < tci_convergence_threshold && ci_count > 4) { break; }
           previous_pivot_error = last_pivot_error;
         }
         // ci_pre.makeCanonical();
